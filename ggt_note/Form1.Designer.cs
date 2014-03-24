12767908f1371f07dd566ccf1195fb7f695a1d6a
DoG-peer <dogeometry50@gmail.com>
2014-03-24 11:50:52 +0900

    ggt_note_0.01

---
+++ ggt_note/Form1.Designer.cs
@@ -0,0 +1,157 @@
+﻿namespace ggt_note
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージ リソースが破棄される場合 true、破棄されない場合は false です。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("MS UI Gothic", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.textBox1.Location = new System.Drawing.Point(5, 10);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(188, 28);
+            this.textBox1.TabIndex = 0;
+            // 
+            // textBox2
+            // 
+            this.textBox2.AcceptsTab = true;
+            this.textBox2.Font = new System.Drawing.Font("MS UI Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.textBox2.Location = new System.Drawing.Point(5, 46);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBox2.Size = new System.Drawing.Size(270, 184);
+            this.textBox2.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("MS UI Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.button1.Location = new System.Drawing.Point(200, 5);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 32);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "検索";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(55, 234);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(24, 24);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "<";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(109, 234);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(24, 24);
+            this.button3.TabIndex = 4;
+            this.button3.Text = ">";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Font = new System.Drawing.Font("MS UI Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.button4.Location = new System.Drawing.Point(139, 235);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(136, 24);
+            this.button4.TabIndex = 5;
+            this.button4.Text = "ネ ッ ト 検 索";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Font = new System.Drawing.Font("MS UI Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.button5.Location = new System.Drawing.Point(5, 234);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(48, 24);
+            this.button5.TabIndex = 6;
+            this.button5.Text = "登録";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.Font = new System.Drawing.Font("MS UI Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.button6.Location = new System.Drawing.Point(82, 234);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(24, 24);
+            this.button6.TabIndex = 7;
+            this.button6.Text = "閉";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 262);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Location = new System.Drawing.Point(5, 5);
+            this.Name = "Form1";
+            this.Text = "ぐぐったノート";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+    }
+}
+
---
+++ ggt_note/Form1.cs
@@ -0,0 +1,199 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+using System.IO;//ファイル書き込み
+using System.Text.RegularExpressions;//正規表現
+
+namespace ggt_note
+{
+    public partial class Form1 : Form
+    {
+        Size smallsize = new Size(300, 80);
+        Size bigsize = new Size(300, 300);
+        nb this_nb;
+        public Form1()
+        {
+            this.MinimumSize = smallsize;
+            this.MaximumSize = smallsize;
+            InitializeComponent();
+            this.Size = smallsize;
+
+            this_nb = new nb();//表示されているnb
+            this_nb.num = -1;
+        }
+
+        public struct nb
+        {
+            public int num;//上から何番目か(0スタート)
+            public string name;//単語
+            public string[] body;//説明
+        }
+
+        private List<nb> analysize()
+        {
+            //ファイルをひらいて
+            string[] text = System.IO.File.ReadAllLines("data.txt");
+            //解析して
+            List<string> lines = new List<string>(text);
+            List<nb> vals = new List<nb>();
+
+            Regex reg = new Regex(@"^\'\'\'(.+)$");
+            int cnt = 0;
+            string tname = "";
+            nb tnb =new nb();
+            List<string> tlines = new List<string>();
+            foreach(string l in text)
+            {
+                Match m = reg.Match(l);//あれ？これなに？
+                if (m.Success)//正規表現を使って判定
+                {//囲いの始め
+                    tname = l.Remove(0,3);
+                    tlines.Add(tname + "とは");
+                    //tname = $1;//m.Groups[0].Value;
+                }
+                else if (l == "\'\'\'")
+                {//囲いの終わり
+                    //追加するtnbの生成
+                    tnb.num = cnt;
+                    tnb.name = tname;
+                    tnb.body = tlines.ToArray();
+                    vals.Add(tnb);
+                    //初期化
+                    cnt++;
+                    tname = "";
+                    tlines.Clear();
+                }
+                else if (tname != "")
+                {//囲われた中身
+                    tlines.Add(l);
+                }//囲いの外は無視
+            }
+            //nbのリストを返す
+            return vals;
+        }
+
+        private List<nb> get_by_name(string name,List<nb> list)
+        {//listの中から名前があうものを持ってくる
+            List<nb> val = new List<nb>();
+            foreach (nb x in list)
+            {
+                if (x.name == name)
+                {
+                    val.Add(x);
+                }
+            }
+            return val;
+        }
+
+        private nb get_nb(string name , int i,string option)
+        {//i番目を基準に表示すべきnbをさがす
+            List<nb> nb_list = analysize();
+            List<nb> nb_list_name_fixed = get_by_name(name,nb_list);
+            
+            nb next_nb = new nb();//i番目からさがして最初
+            nb back_nb = new nb();
+            next_nb.num = -1;
+            back_nb.num = -1;
+            foreach(nb x in nb_list_name_fixed)
+            {
+                if (x.num >= i && next_nb.num == -1)
+                { next_nb = x; }
+                if (x.num < i)
+                { back_nb = x; }
+            }//たまにこれでnextとbackが拾えない
+
+            if (nb_list_name_fixed.Count == 0)
+            {
+                nb error = new nb();
+                error.num = -1;
+                return error;//なかったらエラーとしてnum<0のものをだす
+            }
+            else
+            {
+                switch(option){
+                    case "last":
+                        return nb_list_name_fixed.Last();
+                    case "next":
+                        if (next_nb.num == -1)
+                        { return nb_list_name_fixed[0]; }
+                        else
+                        { return next_nb; }
+                    case "back":
+                        if (back_nb.num == -1)
+                        { return nb_list_name_fixed.Last(); }
+                        else
+                        { return back_nb; }
+                    default:
+                        return nb_list_name_fixed[0];
+                }
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.MinimumSize = bigsize;
+            this.MaximumSize = bigsize;
+            this.Size = bigsize;
+
+            this_nb = get_nb(textBox1.Text,0,"last");
+            if (this_nb.num == -1)
+            {//ない場合
+                System.Diagnostics.Process.Start("https://www.google.co.jp/search?q=" + textBox1.Text);
+            }
+            else
+            {//ある場合
+                textBox2.Lines = this_nb.body;
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            System.Diagnostics.Process.Start("https://www.google.co.jp/search?q=" + textBox1.Text);
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            this.MinimumSize = smallsize;
+            this.MaximumSize = smallsize;
+            this.Size = smallsize;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {//<
+            this_nb = get_nb(this_nb.name,this_nb.num,"back");
+            textBox1.Text = this_nb.name;
+            textBox2.Lines = this_nb.body;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {//>
+            this_nb = get_nb(this_nb.name, this_nb.num + 1, "next");
+            textBox1.Text = this_nb.name;
+            textBox2.Lines = this_nb.body;
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {//登録
+            StreamWriter writer = new StreamWriter("data.txt",true);
+
+            writer.WriteLine("");
+            writer.WriteLine("\'\'\'" + textBox1.Text);
+            foreach(string l in textBox2.Lines)
+            {
+                writer.WriteLine(l);
+            }
+            writer.WriteLine("\'\'\'");
+            writer.WriteLine("");
+            
+            writer.Close();
+        }
+    }
+}
